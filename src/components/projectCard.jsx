a981279c051cd50aa9abd05efdb2542fafb26584
jepalfer <jepalfer2000@gmail.com>
2024-10-22 10:57:55 +0200

    added project

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -5,7 +5,21 @@ import chessUI from '../assets/images/chessUI.jpg'
 function ProjectCard({mode}) {
   return (
     <article className={`Project-card ${mode ? "Dark-mode-project-card" : "Light-mode-project-card"}`}>
-        <div></div>
+        <section className="Project-content">
+          <text className="Title">
+            ELEMENTAL CHESS
+          </text>
+          <section className="Project-technologies">
+
+          </section>
+
+          <article className={`Text ${mode ? "Dark-mode-project-text" : "Light-mode-project-text"}`}>
+            Elemental chess es un videojuego comercial realizado en Unity que consiste en un ajedrez
+            con tipos elementales, cada uno con una o varias debilidades, tal y como si fuera Pokémon.
+
+            La UI ha sido diseñada en su totalidad con Figma.
+          </article>
+        </section>
         <img className={`Project-image ${mode ? "Dark-mode-project-image-border" :"Light-mode-project-image-border" }`} src={chessUI}></img>
         {/* <div></div>
         <div></div>
